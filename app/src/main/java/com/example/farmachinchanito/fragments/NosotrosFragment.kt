c2630f11948e14c00a7a989d003ff77db0fc4d01
K3ll3r95 <50711295+K3ll3r95@users.noreply.github.com>
2019-06-21 22:10:49 -0500

    prueba de subida

---
+++ app/src/main/java/com/example/farmachinchanito/fragments/NosotrosFragment.kt
@@ -0,0 +1,25 @@
+package com.example.farmachinchanito.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.farmachinchanito.R
+
+
+class NosotrosFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_nosotros, container, false)
+    }
+
+
+}
